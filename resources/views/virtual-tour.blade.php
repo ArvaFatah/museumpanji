7b940bed523901a1e994317627e6ffccbba235bb
machmudfends <machmudeffendi@gmail.com>
2022-08-02 19:29:49 +0700

    :sparkles: sidebar menu virtual tour

--- resources/views/virtual-tour.blade.php
+++ resources/views/virtual-tour.blade.php
@@ -1,286 +1,164 @@
-@extends('layouts.artmuseum.base')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Virtual Tour - Museum Panji</title>
+  <link rel="stylesheet" href="{{ asset('assets/landing/css/font-awesome.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('assets/landing/css/bootstrap.css') }}">
+  <style>
+    html, body {
+        width: 100%;
+        height: 100%;
+        overflow: hidden;
+        margin: 0;
+    }
 
-@section('title', 'Home')
+    .pano-image{
+      width: 100%;
+      height: 100%;
+    }
+    #progress {
+      width: 0;
+      height: 5px;
+      position: fixed;
+      top: 0;
+      background: #fff;
+      -webkit-transition: opacity 0.5s ease;
+      transition: opacity 0.5s ease;
+    }
+  
+    #progress.finish {
+      opacity: 0;
+    }
 
-@section('head-link')
-<style>
-  .pano-image{
-    height: 500px;
-  }
-  #progress {
-    width: 0;
-    height: 5px;
-    position: fixed;
-    top: 0;
-    background: #fff;
-    -webkit-transition: opacity 0.5s ease;
-    transition: opacity 0.5s ease;
-  }
-
-  #progress.finish {
-    opacity: 0;
-  }
-</style>
-@endsection
-
-@section('content')
-  <!-- start banner Area -->
-  <!-- <section class="banner-area relative" id="home">	
-    <div class="overlay overlay-bg"></div>
-    <div class="container">
-      <div class="row d-flex align-items-center justify-content-center">
-        <div class="about-content col-lg-12">
-          <h1 class="text-white">
-            Virtual Tour				
-          </h1>	
-          <p class="text-white link-nav"><a href="{{url('landing')}}">Home </a>  <span class="lnr lnr-arrow-right"></span>  <a href="{{url('virtual-tour')}}"> Virtual Tour</a></p>
-        </div>											
+    .template {
+        display: none;
+      }
+  </style>
+</head>
+<body>
+  <div id="progress"></div>
+  
+  <div class="position-fixed mt-3 ml-3">
+    <div class="d-flex flex-row">
+      <div class="card mr-2" style="width: 18rem;">
+        <div class="card-body">
+          <div class="d-flex justify-content-between">
+            <h5 class="card-title">Area Menu</h5>
+          </div>
+          <hr/>
+          <ul class="list-group mb-3">
+            <li class="list-group-item template" style="cursor: pointer;" data-id="">An item</li>
+          </ul>
+          <a href="{{ url('') }}" class="btn btn-secondary btn-block">Beranda</a>
+        </div>
+      </div>
+      <div>
+        <button type="button" class="btn btn-light btn-lg" aria-label="Close">
+          <span aria-hidden="true"><i class="fa fa-close"></i></span>
+        </button>
       </div>
     </div>
-  </section> -->
-  <!-- End banner Area -->
-
-  <div id="progress"></div>
+  </div>
 
   <!-- Start Virtual Tour -->
-  <section id="virtual">
-    <div class="pano-image">
-      <div id="typed"></div>
-    </div>
-  </section>
+  <div class="pano-image">
+    <div id="typed"></div>
+  </div>
   <!-- End Virtual Tour -->
 
-@endsection
+  <!-- Start Scripts -->
+  <script src="{{ asset('assets/landing/js/vendor/jquery-2.2.4.min.js') }}"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+  <script src="{{ asset('assets/landing/js/vendor/bootstrap.min.js') }}"></script>		
+  <script src="{{ asset('assets/custom/js/three.min.js') }}"></script>
+  <script src="{{ asset('assets/custom/js/panolens.min.js') }}"></script>
+  <script src="{{ asset('https://pchen66.github.io/Panolens/js/typed.min.js') }}"></script>
+  <script>
+    const pannoImage = document.querySelector('.pano-image');
+    var list = document.querySelector( '.list-group' );
+    var listItem = document.querySelector( '.list-group-item' );
+    var progressElement = document.getElementById( 'progress' );
+    $(() => {
+      const viewer = new PANOLENS.Viewer({
+        container: pannoImage,
+        output: 'console',
+        initialLookAt: new THREE.Vector3( 0, 0, 5000 )
+      });
+  
+      const arr = @json($virtuals);
+      // console.log(arr);
+      let pano = [];
+      arr.forEach((item, index) => {
 
-@section('script')
-<script src="{{ asset('assets/custom/js/three.min.js') }}"></script>
-<script src="{{ asset('assets/custom/js/panolens.min.js') }}"></script>
-<script src="{{ asset('https://pchen66.github.io/Panolens/js/typed.min.js') }}"></script>
-<script>
-  const pannoImage = document.querySelector('.pano-image');
-// /*
-  var progressElement = document.getElementById( 'progress' );
-  $(() => {
-    const viewer = new PANOLENS.Viewer({
-      container: pannoImage,
-      output: 'console',
-      initialLookAt: new THREE.Vector3( 0, 0, 5000 )
-    });
+        // Add to left panel
+        var itemlist = listItem.cloneNode( true );
+        itemlist.classList.remove( 'template' );
+        itemlist.classList.add( 'vt-item-' + index );
+        itemlist.setAttribute('data-id', item.id);
+        itemlist.textContent = item.judul;
+        list.appendChild( itemlist );
+        $('.vt-item-'+index).data('id', item.id);
 
-    const arr = @json($virtuals);
-    // console.log(arr);
-    let pano = [];
-    arr.forEach((item, index) => {
-      const img = "{{ asset('') }}" + '/' + item.foto ;
-      const panorama = new PANOLENS.ImagePanorama(img);
-      panorama.addEventListener( 'progress', onProgress );
-      panorama.addEventListener( 'enter', onEnter );
-      // console.log('item_' + index, 'vt_'+item.id, typeof pano['vt_'+item.id]);
-      if(typeof pano['vt_'+item.id] == 'undefined'){
-        pano['vt_'+item.id] = panorama;
+        
+        const img = "{{ asset('') }}" + '/' + item.foto ;
+        const panorama = new PANOLENS.ImagePanorama(img);
+        panorama.addEventListener( 'progress', onProgress );
+        panorama.addEventListener( 'enter', onEnter );
+        // console.log('item_' + index, 'vt_'+item.id, typeof pano['vt_'+item.id]);
+        if(typeof pano['vt_'+item.id] == 'undefined'){
+          pano['vt_'+item.id] = panorama;
+        }
+        item.detail.forEach((ditem, dindex) => {
+          // console.log('ditem_' + dindex, 'vt_'+ditem.vid, typeof pano['vt_'+ditem.vid]);
+          const img2 = "{{ asset('') }}" + '/' + ditem.foto;
+          const panorama2 = new PANOLENS.ImagePanorama(img2);
+          if(typeof pano['vt_'+ditem.vid] == 'undefined'){
+            pano['vt_'+ditem.vid] = panorama2;
+          }
+          let angle = new THREE.Vector3(parseInt(ditem.x_axis), parseInt(ditem.y_axis), parseInt(ditem.z_axis) );
+          pano['vt_'+item.id].link( pano['vt_'+ditem.vid], angle, 600);
+          if(dindex == 0){
+            pano['vt_'+item.id].addEventListener( 'enter-fade-start', function(){
+              // set active sidebar menu
+              $('.list-group-item').removeClass('active');
+              $(`li[data-id=${item.id}].list-group-item`).addClass('active');
+              viewer.tweenControlCenter( angle, 12000 );
+            } );
+          }
+        })
+      });
+
+      var panoindex = 0;
+      for(var key in pano){
+        $('.vt-item-'+panoindex).on('click', (event) => {
+          var target = $(event.target)
+          var id = target.data('id')
+          $('.list-group-item').removeClass('active');
+          target.addClass('active')
+          viewer.setPanorama( pano['vt_'+id] )
+        });
+        viewer.add(pano[key]);
+        panoindex += 1;
       }
-      item.detail.forEach((ditem, dindex) => {
-        // console.log('ditem_' + dindex, 'vt_'+ditem.vid, typeof pano['vt_'+ditem.vid]);
-        const img2 = "{{ asset('') }}" + '/' + ditem.foto;
-        const panorama2 = new PANOLENS.ImagePanorama(img2);
-        if(typeof pano['vt_'+ditem.vid] == 'undefined'){
-          pano['vt_'+ditem.vid] = panorama2;
-        }
-        let angle = new THREE.Vector3(parseInt(ditem.x_axis), parseInt(ditem.y_axis), parseInt(ditem.z_axis) );
-        pano['vt_'+item.id].link( pano['vt_'+ditem.vid], angle, 600);
-        if(dindex == 0){
-          pano['vt_'+item.id].addEventListener( 'enter-fade-start', function(){
-            viewer.tweenControlCenter( angle, 12000 );
-          } );
-        }
-      })
-    });
-
-    // viewer.camera.position.set(position.x, position.y, position.z);
-    
-    for(var key in pano){
-      viewer.add(pano[key]);
+  
+    })
+  
+    function onProgress ( event ) {
+      progress = event.progress.loaded / event.progress.total * 100;
+      progressElement.style.width = progress + '%';
+      if ( progress === 100 ) {
+        progressElement.classList.add( 'finish' );
+      }
     }
-
-    // const img = "{{ asset('assets/textures/Depan.jpg') }}";
-    // const img2 = "{{ asset('assets/textures/img2.jpg') }}";
-
-    // const panorama = new PANOLENS.ImagePanorama(img);
-    // const panorama2 = new PANOLENS.ImagePanorama(img2);
-    // panorama.link( panorama2, new THREE.Vector3( 4886.92, -716.46, -720.34 ) );
-    // panorama2.link( panorama, new THREE.Vector3( -2302.98, 358.27, -4414.74 ) );
-    
-    // // First panorama
-    // panorama.addEventListener( 'progress', onProgress );
-    // // panorama.addEventListener( 'load', onWelcomeComplete );
-    // panorama.addEventListener( 'enter', onEnter );
-
-    // const viewer = new PANOLENS.Viewer({
-    //   container: pannoImage,
-    //   output: 'console',
-    //   initialLookAt: new THREE.Vector3( 0, 0, 5000 )
-    // });
-
-    // viewer.add(panorama, panorama2);
-
-  })
-
-  function onProgress ( event ) {
-    progress = event.progress.loaded / event.progress.total * 100;
-    progressElement.style.width = progress + '%';
-    if ( progress === 100 ) {
-      progressElement.classList.add( 'finish' );
+  
+    function onEnter ( event ) {
+      progressElement.style.width = 0;
+      progressElement.classList.remove( 'finish' );
     }
-  }
-
-  function onEnter ( event ) {
-    progressElement.style.width = 0;
-    progressElement.classList.remove( 'finish' );
-  }
-  // */
-
-  /*
-  var panorama, viewer, typed, typed;
-
-  var meterInfospot, valveInfospot, seatInfospot, topboxInfospot, endingInfospot;
-
-  var progressElement = document.querySelector( '#progress' );
-
-  var tweeningDelay = 300, typeStartDelay = 1000, typeSpeed = 50;
-
-  var paragraphs = {
-
-    welcome: [ 'Panolens Presents', 'A Journey to a <strong>Train</strong>', 'Here we go' ],
-    meter: [ 'One of the common meter that you can see on a train', 'it is not specially eye-catching but it just can get your attention', 'it indicates how fast the train is currently going', 'Let us continue' ],
-    valve: [ 'A Valve', 'solid looking and quite decent color', 'here shows the evidence that time would leaves marks on everything', 'same thing applies to our face...' ],
-    seat: [ 'Alright. Here we have a seat. The green pad looks quite comfortable', 'when you are on a train', 'OK. Move on to next one' ],
-    box: [ 'There is a box over there', 'probably some fuse or electronic devices', 'Hmmmm............', 'Anyway', 'Let\'s keep moving' ],
-    ending: [ 'This is our last stop', 'Please continue your journey with this wonder train...' ]
-
-  };
-
-  // Patch for typed.js of cutting back-to-back words
-  for ( var section in paragraphs ) {
-
-    if ( paragraphs.hasOwnProperty( section ) ) {
-
-      paragraphs[ section ].unshift( '' );
-      paragraphs[ section ].push( '' );
-
-    }
-
-  }
-
-  function delayExecute ( func, delay ) {
-
-    setTimeout( func, delay );
-
-  }
-
-  function onLoad () {
-
-    type( paragraphs.welcome, onWelcomeComplete, 2000 );
-
-  }
-
-  function onEnter ( event ) {
-
-    progressElement.style.width = 0;
-    progressElement.classList.remove( 'finish' );
-
-  }
-
-  function onProgress ( event ) {
-
-    var progress = event.progress.loaded / event.progress.total * 100;
-    progressElement.style.width = progress + '%';
-    if ( progress === 100 ) {
-      progressElement.classList.add( 'finish' );
-    }
-
-  }
-
-  function onWelcomeComplete () {
-
-    delayExecute( meterInfospot.focus.bind( meterInfospot ), tweeningDelay );
-    type( paragraphs.meter, onMeterTourComplete );
-    
-  }
-
-  function onMeterTourComplete () {
-
-    delayExecute( valveInfospot.focus.bind( valveInfospot ), tweeningDelay );
-    type( paragraphs.valve, onValveTourComplete );
-
-  }
-
-  function onValveTourComplete () {
-
-    delayExecute( seatInfospot.focus.bind( seatInfospot ), tweeningDelay );
-    type( paragraphs.seat, onSeatTourComplete );
-
-  }
-
-  function onSeatTourComplete () {
-
-    delayExecute( topboxInfospot.focus.bind( topboxInfospot ), tweeningDelay );
-    type( paragraphs.box, onTopboxTourComplete );
-
-  }
-
-  function onTopboxTourComplete () {
-
-    delayExecute( endingInfospot.focus.bind( endingInfospot ), tweeningDelay );
-    type( paragraphs.ending, function(){ viewer.OrbitControls.enabled = true; } );
-
-  }
-
-  function type ( stringArray, onComplete, startDelay ) {
-
-    onComplete = onComplete || function(){};
-    startDelay = startDelay >= 0 ? startDelay : typeStartDelay;
-
-    typed = new Typed( "#typed", {
-
-      strings: stringArray,
-      typeSpeed: typeSpeed,
-      showCursor: false,
-      startDelay: startDelay,
-      onComplete: onComplete
-      
-    });
-
-  }
-
-  // Infospots
-  meterInfospot = new PANOLENS.Infospot();
-  valveInfospot = new PANOLENS.Infospot();
-  seatInfospot = new PANOLENS.Infospot();
-  topboxInfospot = new PANOLENS.Infospot();
-  endingInfospot = new PANOLENS.Infospot( 10e-7 );  // Make it not visible
-
-  meterInfospot.position.set( 4610.04, 1280.07, 1431.29 );
-  valveInfospot.position.set( 4637.61, -798.12, -1671.24 );
-  seatInfospot.position.set( 1934.61, -2611.69, -3792.91 );
-  topboxInfospot.position.set( -3348.82, 3705.92, 45.54 );
-  endingInfospot.position.set( -3461.4, -3592.37, -241.38 );
-
-  meterInfospot.addHoverText( 'Speedometer', 50 );
-  valveInfospot.addHoverText( 'Valve', 50 );
-  seatInfospot.addHoverText( 'Seat', 50 );
-  topboxInfospot.addHoverText( 'Box', 50 );
-
-  // Panorama
-  panorama = new PANOLENS.ImagePanorama( "{{asset('assets/textures/Depan.jpg')}}" );
-  panorama.addEventListener( 'progress', onProgress );
-  panorama.addEventListener( 'load', onLoad );
-  panorama.addEventListener( 'enter', onEnter );
-  panorama.add( meterInfospot, valveInfospot, seatInfospot, topboxInfospot, endingInfospot );
-
-  Viewer
-  viewer = new PANOLENS.Viewer( { container: pannoImage, controlBar: false, initialLookAt: new THREE.Vector3( 0, 0, 5000 ) } );
-  viewer.OrbitControls.enabled = false;
-  viewer.add( panorama );
-  */
-</script>
-@endsection
+  </script>
+  <!-- End Scripts -->
+</body>
+</html>
